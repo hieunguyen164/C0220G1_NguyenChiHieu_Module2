1be97470d9c5c6ac03657bf001af97e09210d7f7
hieunguyen164 <hieunguyen16491@gmail.com>
2020-04-07 17:17:03 +0700

    Thuc Hanh Module 2

---
+++ Module2/src/b1_ngonngulaptrinhjava/baitap/HienThiLoiChao.java
@@ -0,0 +1,12 @@
+package b1_ngonngulaptrinhjava.baitap;
+
+import java.util.Scanner;
+
+public class HienThiLoiChao {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = input.nextLine();
+        System.out.println("Hello: "+name);
+    }
+}
