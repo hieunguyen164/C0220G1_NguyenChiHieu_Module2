2d9078513dbac68b53e1fc1e21ca87e9d363421e
hieunguyen164 <hieunguyen16491@gmail.com>
2020-04-17 15:18:47 +0700

    Bai Tap Module 2

--- Project_JavaCore/src/b7_cleancodevarefactoring/baitap/TennisGame.java
+++ Project_JavaCore/src/b7_cleancodevarefactoring/baitap/TennisGame.java
@@ -4,20 +4,65 @@ public class TennisGame {
 
     public static String getScore(String player1Name, String player2Name, int player1Score, int player2Score) {
         String score = "";
-        boolean isTie = player1Score == player2Score;
-        if (isTie) {
+        boolean isDeuce = player1Score == player2Score;
+        boolean getMaxScore = player1Score >= 4 || player2Score >= 4;
+        if (isDeuce)
+        {
             score = getResult(player1Score);
-        } else if (player1Score >= 4 || player2Score >= 4) {
-            score = getWinner(player1Score, player2Score);
-        } else {
-            score = getRatio(player1Score, player2Score, score);
+        }
+        else {
+
+            if (getMaxScore)
+            {
+                score = getWinner(player1Score, player2Score);
+            }
+            else
+            {
+                score = getRatio(player1Score, player2Score, score);
+            }
         }
         return score;
     }
 
-    public static String getResult(int player1Score) {
+    private static String getRatio(int player1Score, int player2Score, String score) {
+        int tempScore;
+        for (int i = 1; i<3; i++)
+        {
+            if (i==1) tempScore = player1Score;
+            else { score+="-"; tempScore = player2Score;}
+            switch(tempScore)
+            {
+                case 0:
+                    score+="Love";
+                    break;
+                case 1:
+                    score+="Fifteen";
+                    break;
+                case 2:
+                    score+="Thirty";
+                    break;
+                case 3:
+                    score+="Forty";
+                    break;
+            }
+        }
+        return score;
+    }
+
+    private static String getWinner(int player1Score, int player2Score) {
         String score;
-        switch (player1Score) {
+        int minusResult = player1Score-player2Score;
+        if (minusResult==1) score ="Advantage player1";
+        else if (minusResult ==-1) score ="Advantage player2";
+        else if (minusResult>=2) score = "Win for player1";
+        else score ="Win for player2";
+        return score;
+    }
+
+    private static String getResult(int player1Score) {
+        String score;
+        switch (player1Score)
+        {
             case 0:
                 score = "Love-All";
                 break;
@@ -33,48 +78,7 @@ public class TennisGame {
             default:
                 score = "Deuce";
                 break;
-        }
-        return score;
 
-    }
-
-    public static String getWinner(int player1Score, int player2Score) {
-        String score;
-        int minusResult = player1Score - player2Score;
-        if (minusResult == 1) {
-            score = "Advantage player1";
-        } else if (minusResult == -1) {
-            score = "Advantage player2";
-        } else if (minusResult >= 2) {
-            score = "Win for player1";
-        } else {
-            score = "Win for player2";
-        }
-        return score;
-    }
-
-    public static String getRatio(int player1Score, int player2Score, String score) {
-        int tempScore;
-        for (int i = 1; i < 3; i++) {
-            if (i == 1) tempScore = player1Score;
-            else {
-                score += "-";
-                tempScore = player2Score;
-            }
-            switch (tempScore) {
-                case 0:
-                    score += "Love";
-                    break;
-                case 1:
-                    score += "Fifteen";
-                    break;
-                case 2:
-                    score += "Thirty";
-                    break;
-                case 3:
-                    score += "Forty";
-                    break;
-            }
         }
         return score;
     }
