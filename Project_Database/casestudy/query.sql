ebb47171ef4e354d4887722fd6ee6336b4811632
hieunguyen164 <hieunguyen16491@gmail.com>
2020-05-03 20:41:08 +0700

    casestudy database

--- Project_Database/casestudy/query.sql
+++
@@ -1,9 +0,0 @@
--- Task 2
-select *
-from nhanvien
-where ten like '[htk]%' and length(concat(ho,' ',ten)) <= 15 ;
-
--- Task 3
-select *  
-from khachhang
-where diaChi in ('da nang','quang tri') and year(ngaySinh) <= 2002 and year(ngaySinh) >= 1970;
